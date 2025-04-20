e9efcf014d5d2fbd6fc7885b508a9bc572c11bc8
Jackson <83289186+Packjackisback@users.noreply.github.com>
2025-04-19 22:03:58 -0500

    not again

--- main.sh
+++ main.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+BASE_DIR=$(pwd)
 
-cd modules
+cd "$BASE_DIR/modules"
 ./00-initial_setup.sh
 source 01-create_user.sh
-su - "$username" -c ./02-install_packages.sh
-su - "$username" -c ./03-enable_sddm.sh
+su - "$username" -c "$BASE_DIR/modules/02-install_packages.sh"
+su - "$username" -c "$BASE_DIR/modules/03-enable_sddm.sh"
+
+echo "Setup Complete! Reboot"
