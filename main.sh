2ed5191f1b8dca4e2fbdf905b8d413be340b78db
Jackson <83289186+Packjackisback@users.noreply.github.com>
2025-04-19 22:06:39 -0500

    im an idiot (this might be why i have a lot of commits)

--- main.sh
+++ main.sh
@@ -3,8 +3,8 @@ BASE_DIR=$(pwd)
 
 cd "$BASE_DIR/modules"
 ./00-initial_setup.sh
-source 01-create_user.sh
-su - "$username" -c "$BASE_DIR/modules/02-install_packages.sh"
-su - "$username" -c "$BASE_DIR/modules/03-enable_sddm.sh"
+./01-create_user.sh
+./02-install_packages.sh
+./03-enable_sddm.sh"
 
 echo "Setup Complete! Reboot"
