eec5812be88ded25a12bcb53ad8a33b719f43f6d
Jackson <83289186+Packjackisback@users.noreply.github.com>
2025-04-19 22:15:24 -0500

    oops

--- modules/02-install_packages.sh
+++ modules/02-install_packages.sh
@@ -2,6 +2,7 @@ if [[ $EUID -ne 0 ]]; then
   echo "not root (???)"
   exit 1
 fi
-
+sudo pacman -S reflector
+reflector
 sudo pacman -S base-devel linux-headers plasma kde-applications sddm --noconfirm
 
