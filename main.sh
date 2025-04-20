6764ad940cb297089b68b797836efc3362099f65
Jackson <83289186+Packjackisback@users.noreply.github.com>
2025-04-19 21:59:42 -0500

    not again

--- main.sh
+++ main.sh
@@ -3,6 +3,5 @@
 cd modules
 ./00-initial_setup.sh
 source 01-create_user.sh
-su - "$username"
-./02-install_packages.sh
-./03-enable_sddm.sh
+su - "$username" -c ./02-install_packages.sh
+su - "$username" -c ./03-enable_sddm.sh
